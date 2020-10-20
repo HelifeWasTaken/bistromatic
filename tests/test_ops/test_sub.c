1dd024ada7be615d902d8de514a5dba487cb0154
Ludovic PELTIER <civodulap@localhost.localdomain>
2020-10-20 08:40:02 -0400

    dup str test

--- tests/test_ops/test_sub.c
+++ tests/test_ops/test_sub.c
@@ -13,25 +13,25 @@ char *my_sub(char const *s1, char const *s2);
 
 Test(my_sub, five_minus_five)
 {
-    cr_assert_str_eq(my_sub("5", "5"), "0");
+    cr_assert_str_eq(my_sub(my_strdup("5"), my_strdup("5")), "0");
 }
 
 Test(my_sub, five_minus_six)
 {
-    cr_assert_str_eq(my_sub("5", "6"), "-1");
+    cr_assert_str_eq(my_sub(my_strdup("5"), my_strdup("6")), "-1");
 }
 
 Test(my_sub, six_minus_five)
 {
-    cr_assert_str_eq(my_sub("6", "5"), "1");
+    cr_assert_str_eq(my_sub(my_strdup("6"), my_strdup("5")), "1");
 }
 
 Test(my_sub, sub_high_numbered)
 {
-    cr_assert_str_eq(my_sub("45005", "5858"), "39147");
+    cr_assert_str_eq(my_sub(my_strdup("45005"), my_strdup("5858")), "39147");
 }
 
 Test(my_sub, sub_high_numbered_neg)
 {
-    cr_assert_str_eq(my_sub("5858", "45005"), "-39147");
+    cr_assert_str_eq(my_sub(my_strdup("5858"), my_strdup("45005")), "-39147");
 }
