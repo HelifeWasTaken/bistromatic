9fb618b02d6a2562669f4630fa94b3613c7df9be
Alexis Norech <alexis.cheron974@gmail.com>
2020-10-20 14:01:04 +0400

    fixed is_first_smaller

--- lib/numformat/is_first_smaller.c
+++ lib/numformat/is_first_smaller.c
@@ -10,9 +10,14 @@ int is_first_smaller(char const *s1, char const *s2)
 {
     int len_s1 = my_strlen(s1);
     int len_s2 = my_strlen(s2);
+    int i = 0;
 
-    if (len_s1 != len_s2) {
+    if (len_s1 != len_s2) 
         return (len_s1 < len_s2);
-    }   
-    return (s1[0] < s2[0]);
+    while (s1[i] != '\0') {
+        if (s1[i] != s2[i])
+            return (s1[i] < s2[i]);
+        i++;
+    }
+    return (0);
 }
