35f5dcf7b69f7175e8ba144e8142ab458e4f110d
Ludovic PELTIER <civodulap@localhost.localdomain>
2020-10-20 06:37:10 -0400

    test revisé

--- tests/test_ops/test_mult.c
+++ tests/test_ops/test_mult.c
@@ -13,29 +13,29 @@ char *my_mult(char *s1, char *s2);
 
 Test(my_mult, high_numbered_mult)
 {
-    cr_assert_eq(my_mult("0000000005", "2000000000"), "10000000000");
+    cr_assert_str_eq(my_mult("0000000005", "2000000000"), "10000000000");
 }
 
 Test(my_mult, high_low_nubered)
 {
-    cr_assert_eq(my_mult("5", "6"), "30");
+    cr_assert_str_eq(my_mult("5", "6"), "30");
 }
 
 Test(my_mult, one_low_neg_numbered)
 {
-    cr_assert_eq(my_mult("2843", "02-"), "-69640");
+    cr_assert_str_eq(my_mult("2843", "02-"), "-69640");
 }
 
 Test(my_mult, two_neg_number)
 {
-    cr_assert_eq(my_mult("-003", "-054"), "135000");
+    cr_assert_str_eq(my_mult("-003", "-054"), "135000");
 }
 
 Test(my_mult, high_numbered_mult_neg)
 {
-    cr_assert_eq(my_mult("0000000005-", "2000000000"), "-10000000000");
+    cr_assert_str_eq(my_mult("0000000005-", "2000000000"), "-10000000000");
 }
 Test(my_mult, high_numbered_mult_two_neg)
 {
-    cr_assert_eq(my_mult("0000000005-", "2000000000-"), "10000000000");
+    cr_assert_str_eq(my_mult("0000000005-", "2000000000-"), "10000000000");
 }
