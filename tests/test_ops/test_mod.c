ffd18daead491c5803c083549ce795474a150059
Ludovic PELTIER <civodulap@localhost.localdomain>
2020-10-20 05:42:34 -0400

    unit test moddulo

---
+++ tests/test_ops/test_mod.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2020
+** test_mod
+** File description:
+** make unit test of modulo function*
+*/
+
+#include <criterion/criterion.h>
+#include <my_numformat.h>
+
+char my_mod(char *s1, char *s2);
+
+Test(my_mod, test_modulo_two)
+{
+    cr_assert_eq(my_mod("2", "2"), "0");
+}
+
+Test(my_mod, test_modulo_dix)
+{
+    cr_assert_eq(my_mod("254", "10"), "25");
+}
+
+Test(my_mod, Test_modulo_high_numbered)
+{
+    cr_assert_eq(my_mod("5848821441", "25454555"), "19728346");
+}
