1cc253e2165b02a8d0cfba251c7df4c2781425bd
mattis dalleau <mattis.dalleau@epitech.eu>
2020-10-20 09:52:18 +0400

    lighter op parser

--- do_op.c
+++ do_op.c
@@ -13,7 +13,7 @@ int op_parser(char *s1, char *s2, char sign)
     for (int i = 0; array_ops[i].calculation_process != 0; i++) {
         if (array_ops[i].operation == sign) {
             calc_func = array_ops[i].calculation_process;
-            calc_func(my_revstr(my_strdup(s1)), my_revstr(my_strdup(s2));
+            calc_func(s1, s2);
         }
     }
     return (0);
