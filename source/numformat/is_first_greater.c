4486a89b716d0caa5c06b23b89845c1a4211f0af
mattis dalleau <mattis.dalleau@epitech.eu>
2020-10-27 11:41:56 +0400

    removing surcheck in the if conditions coverage 100

--- source/numformat/is_first_greater.c
+++ source/numformat/is_first_greater.c
@@ -16,14 +16,14 @@ bool is_first_greater(char const *s1, char const *s2)
     char *s1_dup = NULL;
     char *s2_dup = NULL;
 
-    if (get_sign(s1) == -1 && get_sign(s2) == 1)
-        return (false);
-    if (get_sign(s1) == 1 && get_sign(s2) == -1)
-        return (true);
     if (get_sign(s1) == -1 && get_sign(s2) == -1) {
         s1_dup = invert_sign(my_strdup(s1));
         s2_dup = invert_sign(my_strdup(s2));
         return (is_first_smaller(s1_dup, s2_dup));
     }
+    if (get_sign(s1) == -1)
+        return (false);
+    if (get_sign(s2) == -1)
+        return (true);
     return (is_first_greater_unsigned(s1, s2));
 }
